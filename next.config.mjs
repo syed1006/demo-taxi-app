bf9d0512f746c8b7e42e41c1de2c4b3911fddb9e
syedgaian <syedgaian@users.noreply.github.com>
2025-07-07 18:42:57 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	basePath: "/demo-taxi-app",
+	output: "export",
+	distDir: "build",
+	reactStrictMode: true,
+	assetPrefix: "https://syedgaian.github.io/demo-taxi-app/",
+};
 
-export default nextConfig
+export default nextConfig;
